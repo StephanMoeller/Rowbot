c8fed66f00e0cfb8a6bcdc7dfaea38294a971fed
Stephan Møller <stephanryer@hotmail.com>
2023-02-14 20:52:59 +0100

    CsvHelper source and target

---
+++ src/Rowbot.CsvHelper/CsvHelperSource.cs
@@ -0,0 +1,55 @@
+﻿using CsvHelper;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Rowbot.CsvHelper
+{
+    public class CsvHelperSource : RowSource
+    {
+        private readonly CsvReader _csvReader;
+        public CsvHelperSource(CsvReader csvReader)
+        {
+            _csvReader = csvReader;
+        }
+
+        public override void Dispose()
+        {
+            _csvReader.Dispose();
+        }
+
+        protected override void OnComplete()
+        {
+            // Nothing to complete
+        }
+
+        protected override ColumnInfo[] OnInitAndGetColumns()
+        {
+            _csvReader.Read();
+            _csvReader.ReadHeader();
+            if (_csvReader.HeaderRecord == null)
+            {
+                throw new Exception("No headers detected in csv file");
+            }
+
+            var columns = _csvReader.HeaderRecord.Select(header => new ColumnInfo(name: header, valueType: typeof(string))).ToArray();
+            return columns;
+        }
+
+        protected override bool OnReadRow(object[] values)
+        {
+            if (_csvReader.Read())
+            {
+                for (var i = 0; i < values.Length; i++)
+                {
+                    values[i] = _csvReader.GetField(i);
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Rowbot.CsvHelper/CsvHelperTarget.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rowbot.CsvHelper
+{
+    public class CsvHelperTarget : RowTarget
+    {
+        private readonly CsvWriter _csvWriter;
+
+        public CsvHelperTarget(CsvWriter csvWriter)
+        {
+            _csvWriter = csvWriter;
+        }
+
+        public override void Dispose()
+        {
+            _csvWriter.Dispose();
+        }
+
+        protected override void OnComplete()
+        {
+            _csvWriter.Flush();
+        }
+
+        protected override void OnInit(ColumnInfo[] columns)
+        {
+            _csvWriter.WriteRecords(columns.Select(c => c.Name).ToArray());
+        }
+
+        protected override void OnWriteRow(object[] values)
+        {
+            _csvWriter.WriteRecords(values);
+        }
+    }
+}
