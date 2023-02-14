bd99b662b0cd7f4132db79c760d90362fccf0981
Stephan Møller <stephanryer@hotmail.com>
2023-02-14 15:15:02 +0100

    Removed custom csv target to be replaced with CsvHelper

--- adhoc/Program.cs
+++ adhoc/Program.cs
@@ -8,23 +8,7 @@ namespace AdhocConsole
     {
         static void Main(string[] args)
         {
-            var elements = Enumerable.Range(0, 10).Select(e => new
-            {
-                Name = "Hello there and wel\"come! ",
-                Age = (short)29,
-                Age2 = (int)9898,
-                Age3 = (long)98989898
-            });
-
-            using (var fs = File.Create("c:\\temp\\output.csv"))
-            using (var source = PropertyReflectionSource.Create(elements))
-            using (var target = new CsvTarget(outputStream: fs, new CsvConfig()))
-            {
-                RowbotExecutor.ExecuteDualThreaded(source, target, maxQueueLength: 2);
-            }
-
-            MiniExcel.SaveAs(path: "c:\\temp\\MiniExcel.csv", value: elements, printHeader: true, excelType: ExcelType.CSV);
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
