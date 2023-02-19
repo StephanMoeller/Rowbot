10c9c2ba6a6c6f7ec8c508838ed5ab6ab317ec5f
Stephan Møller <stephanryer@hotmail.com>
2023-02-19 21:07:05 +0100

    Added adhoc program

--- adhoc/Program.cs
+++ adhoc/Program.cs
@@ -1,6 +1,9 @@
-﻿using Rowbot.Execution;
+﻿using Rowbot.CsvHelper;
+using Rowbot.Execution;
 using Rowbot.Sources;
 using Rowbot.Targets;
+using Rowbot;
+using System.Globalization;
 
 namespace AdhocConsole
 {
@@ -8,7 +11,24 @@ namespace AdhocConsole
     {
         static void Main(string[] args)
         {
-            
+            var config = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" };
+            using (var fs = File.Create("c:\\temp\\output.csv"))
+            {
+                using (var target = new CsvHelperTarget(stream: fs, configuration: config, writeHeaders: true))
+                {
+                    target.Init(new Rowbot.ColumnInfo[]{
+                        new ColumnInfo(name: "Col1", typeof(string)),
+                        new ColumnInfo(name: "Col2", typeof(string)),
+                        new ColumnInfo(name: "Col3", typeof(string))
+                    });
+                    var values = new object[] { new string('a', 300), new string('b', 300), new string('c', 300) };
+                    for(var i = 0; i < 1_000_000; i++)
+                    {
+                        target.WriteRow(values);
+                    }
+                }
+            }
+
         }
     }
 }
