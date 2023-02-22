5f6270f47783ef656317208e4079d04ae282e08a
Stephan Møller <stephanryer@hotmail.com>
2023-02-22 15:13:41 +0100

    Added dynamic object target

---
+++ src/Rowbot.Core/Targets/DynamicObjectTarget.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Rowbot.Core.Targets
+{
+    public class DynamicObjectTarget : RowTarget
+    {
+        private string[] _columnNames;
+        private LinkedList<dynamic> _dynamicList = new LinkedList<dynamic>();
+        public override void Dispose()
+        {
+            
+        }
+
+        protected override void OnComplete()
+        {
+            
+        }
+
+        public LinkedList<dynamic> GetResult()
+        {
+            return _dynamicList;
+        }
+
+        protected override void OnInit(ColumnInfo[] columns)
+        {
+            _columnNames = columns.Select(c => c.Name).ToArray();
+        }
+
+        protected override void OnWriteRow(object[] values)
+        {
+            IDictionary<string, object> newObject = new ExpandoObject();
+            for (var i = 0; i < values.Length; i++)
+            {
+                newObject.Add(_columnNames[i], values[i]);
+            }
+            _dynamicList.AddLast(newObject);
+        }
+    }
+}
---
+++ tests/Rowbot.Core.Test/Targets/DynamicObjectTarget_Test.cs
@@ -0,0 +1,78 @@
+﻿using Rowbot.Core.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rowbot.Test.Targets
+{
+    public class DynamicObjectTarget_Test
+    {
+        [Fact]
+        public void NoColumns_Test()
+        {
+            using (var target = new DynamicObjectTarget())
+            {
+                target.Init(new ColumnInfo[0]);
+
+                target.WriteRow(new object[0]);
+
+                target.Complete();
+
+                var result = target.GetResult();
+                Assert.Single(result);
+            }
+        }
+
+        [Fact]
+        public void NoRows_Test()
+        {
+            using (var target = new DynamicObjectTarget())
+            {
+                target.Init(new ColumnInfo[]{
+                    new ColumnInfo(name: "A", typeof(string))
+                });
+
+                target.Complete();
+
+                Assert.Empty(target.GetResult());
+            }
+        }
+
+        [Fact]
+        public void WithRowsAndColumns_Test()
+        {
+            using (var target = new DynamicObjectTarget())
+            {
+                target.Init(new ColumnInfo[]{
+                    new ColumnInfo(name: "A", typeof(string)),
+                    new ColumnInfo(name: "B", typeof(int)),
+                    new ColumnInfo(name: "SomeOtherProperty", typeof(int?))
+                });
+
+                target.WriteRow(new object[] { "Hello", 42, 82 });
+                target.WriteRow(new object?[] { null, 42, null });
+
+                target.Complete();
+
+                var result = target.GetResult().ToArray();
+                Assert.Equal(2, result.Length);
+
+                {
+                    var e = result[0];
+                    Assert.Equal("Hello", e.A);
+                    Assert.Equal(42, e.B);
+                    Assert.Equal(82, e.SomeOtherProperty);
+                }
+
+                {
+                    var e = result[1];
+                    Assert.Equal(null, e.A);
+                    Assert.Equal(42, e.B);
+                    Assert.Equal(null, e.SomeOtherProperty);
+                }
+            }
+        }
+    }
+}
