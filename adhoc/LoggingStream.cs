750e06242d1c36ba77bf97335311645c908cb155
Stephan Møller <sr@inmobile.dk>
2023-03-02 16:12:09 +0100

    POC with no seek on streams with SharpZipLib

---
+++ adhoc/LoggingStream.cs
@@ -0,0 +1,101 @@
+﻿namespace AdhocConsole
+{
+    public class LoggingStream : Stream
+    {
+        private readonly Stream _innerStream;
+        private readonly Action<string> _logger;
+
+        public LoggingStream(Stream innerStream, Action<string> logger)
+        {
+            _innerStream = innerStream;
+            _logger = logger;
+        }
+
+        public override bool CanRead
+        {
+            get
+            {
+                _logger("CanRead");
+                return _innerStream.CanRead;
+            }
+        }
+
+        public override bool CanSeek
+        {
+            get
+            {
+                _logger("CanSeek");
+                return false;
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                _logger("CanWrite");
+                return _innerStream.CanWrite;
+            }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                _logger("Length");
+                return _innerStream.Length;
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                _logger("Get Position");
+                return _innerStream.Position;
+            }
+            set
+            {
+                _logger("Set Position");
+                _innerStream.Position = value;
+            }
+        }
+
+        public override void Close()
+        {
+            _logger("Flush");
+
+            _innerStream.Close();
+        }
+
+        public override void Flush()
+        {
+            _logger("Flush");
+            _innerStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            _logger("Read");
+            return _innerStream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            _logger("Seek");
+            return _innerStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            _logger("SetLength");
+            _innerStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _logger("Write");
+            _innerStream.Write(buffer, offset, count);
+        }
+    }
+}
--- adhoc/Program.cs
+++ adhoc/Program.cs
@@ -2,6 +2,13 @@
 using Rowbot.Core.Targets;
 using BenchmarkDotNet.Attributes;
 using Rowbot.Core.Execution;
+using System.IO.Compression;
+using System;
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Presentation;
+using System.Text.Unicode;
+using System.Text;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace AdhocConsole
 {
@@ -9,29 +16,66 @@ namespace AdhocConsole
     {
         static void Main(string[] args)
         {
-            
-            using (var outputStream = File.Create("c:\\temp\\test1.xlsx"))
-            // using (var outputStream = new MemoryStream())
+            //using (FileStream zipToOpen = new FileStream("c:\\temp\\hello.zip", FileMode.Create))
+            //{
+            //    using (ZipArchive archive = new ZipArchive(new LoggingStream(zipToOpen, logMsg => Console.WriteLine(logMsg)), ZipArchiveMode.Update))
+            //    {
+            //        ZipArchiveEntry readmeEntry = archive.CreateEntry("Readme.txt");
+            //        using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
+            //        {
+            //            writer.WriteLine("Information about this package.");
+            //            writer.WriteLine("========================");
+            //        }
+            //    }
+            //}
+
+
+
+
+
+
+
+            // 'using' statements guarantee the stream is closed properly which is a big source
+            // of problems otherwise.  Its exception safe as well which is great.
+            using (ZipOutputStream OutputStream = new ZipOutputStream(new LoggingStream(File.Create("c:\\temp\\hello2.zip"), logMsg => Console.WriteLine(logMsg))))
             {
-                //using (var target = new ClosedXmlExcelTarget(outputStream: outputStream, sheetName: "SheetName", writeHeaders: true, leaveOpen: true))
-                using (var target = new ExcelTarget(outputStream: outputStream, sheetName: "MySheet", writeHeaders: false, leaveOpen: true))
-                {
-                    int colNum = 1;
+                // Define the compression level
+                // 0 - store only to 9 - means best compression
+                OutputStream.SetLevel((int)CompressionLevel.NoCompression);
 
-                    var range = Enumerable.Range(0, colNum);
-                    target.Init(range.Select(num => new ColumnInfo(name: "Col" + num, valueType: typeof(decimal))).ToArray());
+                byte[] buffer = new byte[4096];
 
-                    object[] data = range.Select(num => 123.45678m).Cast<object>().ToArray();
+                // Using GetFileName makes the result compatible with XP
+                // as the resulting path is not absolute.
+                ZipEntry entry = new ZipEntry("Hello.txt");
 
-                    for (var i = 0; i < colNum; i++)
-                    {
-                        target.WriteRow(data);
-                    }
+                // Setup the entry data as required.
 
-                    target.Complete();
-                }
-                outputStream.Close();
+                // Crc and size are handled by the library for seakable streams
+                // so no need to do them here.
+
+                // Could also use the last write time or similar for the file.
+                entry.DateTime = DateTime.Now;
+                OutputStream.PutNextEntry(entry);
+
+                var bytes = Encoding.UTF8.GetBytes("Hello!");
+                OutputStream.Write(bytes, 0, bytes.Length);
+
+                // Finish/Close arent needed strictly as the using statement does this automatically
+
+                // Finish is important to ensure trailing information for a Zip file is appended.  Without this
+                // the created file would be invalid.
+                OutputStream.Finish();
+
+                // Close is important to wrap things up and unlock the file.
+                OutputStream.Close();
+
+                Console.WriteLine("Files successfully compressed");
             }
+
+
+
+
 
             Console.WriteLine("done");
             Console.ReadLine();
@@ -42,7 +86,7 @@ namespace AdhocConsole
     public class CsvBenchmark
     {
         [Benchmark]
-        public void V1() => RunInternal(outputStream => new CsvTarget(outputStream: outputStream, new CsvConfig(){ }));
+        public void V1() => RunInternal(outputStream => new CsvTarget(outputStream: outputStream, new CsvConfig() { }));
 
         //[Benchmark]
         //public void V2() => RunInternal(outputStream => new CsvTargetV2(outputStream: outputStream, new CsvConfig() { }));
